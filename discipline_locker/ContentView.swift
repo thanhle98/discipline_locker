98eb559444a2fad7fb50e84b2fb3b0f62ae30c05
Thanh Le <thanhlx1298@gmail.com>
2026-04-24 09:45:23 +0700

    Initial Commit

---
+++ discipline_locker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  discipline_locker
+//
+//  Created by  Le Xuan Thanh on 24/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ discipline_locker/discipline_lockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  discipline_lockerApp.swift
+//  discipline_locker
+//
+//  Created by  Le Xuan Thanh on 24/4/26.
+//
+
+import SwiftUI
+
+@main
+struct discipline_lockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
